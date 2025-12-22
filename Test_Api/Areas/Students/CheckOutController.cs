f77e5815b6922ec4388a229c6d6eaa3cc341e579
Future <Future@DESKTOP-0PKRDV3>
2025-12-22 12:05:53 -1000

    ,,

--- Test_Api/Areas/Students/CheckOutController.cs
+++ Test_Api/Areas/Students/CheckOutController.cs
@@ -14,6 +14,7 @@ namespace Test_Api.Areas.Students
     [Route("[Area]/[controller]")]
     [ApiController]
     [Area("Students")]
+    [Authorize]
 
     public class CheckOutController : ControllerBase
     {
